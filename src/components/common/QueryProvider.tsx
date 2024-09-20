ba4a00e4a4ee8a195212ca820758a70cb6a25e4e
KiHoon Roh <started677@gmail.com>
2024-09-20 15:40:39 +0900

    chore: tanstack query 설정
    - devtools 설치

---
+++ src/components/common/QueryProvider.tsx
@@ -0,0 +1,25 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+interface QueryProviderProps {
+  children: React.ReactNode;
+}
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+const QueryProvider = ({ children }: QueryProviderProps) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+export default QueryProvider;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
 import { createRoot } from "react-dom/client";
 import App from "App";
 
+import QueryProvider from "components/common/QueryProvider";
+
 // Tailwind
 import "./tailwind.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <QueryProvider>
+    <App />
+  </QueryProvider>
+);
