ef9e303f3a93bc6bc232b905d93fdb8ec6b4564a
KiHoon Roh <started677@gmail.com>
2024-09-21 11:15:09 +0900

    chore: eslint, prettier 설정

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'react/jsx-uses-react': 0,
+    'react/react-in-jsx-scope': 0,
+    'react-hooks/exhaustive-deps': 1,
+    // '@typescript-eslint/no-explicit-any': 0,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  ignorePatterns: ['node_modules/', 'dist/', '*.config.*', '.eslintrc.cjs'],
+};
