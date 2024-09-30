23647f148818dcd4f8c9b0958f04cc26080972aa
KiHoon Roh <started677@gmail.com>
2024-09-30 14:46:18 +0900

    feat: Home Page 구현

--- src/layout/GlobalLayout.tsx
+++ src/layout/GlobalLayout.tsx
@@ -1,17 +1,19 @@
 import { Outlet } from 'react-router-dom';
 
 // Components
+import ModalRenderer from 'components/Modal/ModalRenderer';
 import Header from 'components/common/Header';
 import Footer from 'components/common/Footer';
 
 const GlobalLayout = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex min-h-screen flex-col">
       <Header />
-      <main className="flex-grow container mx-auto px-4 mt-[72px] mb-[28px]">
+      <main className="mt-[5rem] min-h-[calc(100vh_-_150px)] flex-grow p-6 md:px-16 lg:px-24">
         <Outlet />
       </main>
       <Footer />
+      <ModalRenderer />
     </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,24 @@
+import Button from 'components/atoms/Button';
+import { Link } from 'react-router-dom';
+
 const Home = () => {
   return (
-    <div>
-      <h1>홈페이지</h1>
-    </div>
+    <main>
+      <section className="mb-12 rounded-lg bg-white p-8 shadow-lg">
+        <h2 className="mb-4 text-[5rem] font-bold text-blue-800">
+          AI 퀴즈로
+          <br />
+          당신의 <span className="text-green-500">CS 실력</span>을<br />
+          레벨업하세요
+        </h2>
+        <p className="mb-6 text-gray-600"></p>
+        <Link to="select-topics">
+          <Button variant="primary" size="md">
+            시작하기
+          </Button>
+        </Link>
+      </section>
+    </main>
   );
 };
 
