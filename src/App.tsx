a196153ec55cc276b33a0c5a26082c5aaa173855
KiHoon Roh <started677@gmail.com>
2024-09-30 14:55:46 +0900

    fix: 누락된 Suspense 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { Suspense } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { router } from 'router';
 
+import LoadingPage from 'pages/Loading';
+
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <Suspense fallback={<LoadingPage />}>
+      <RouterProvider router={router} />
+    </Suspense>
+  );
 }
 
 export default App;
