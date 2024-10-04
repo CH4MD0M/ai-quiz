7e85b551ef4b6bc30e437a63baaaafc50c6580be
KiHoon Roh <started677@gmail.com>
2024-10-04 11:57:14 +0900

    fix: api url 수정

--- src/api/getTopics.ts
+++ src/api/getTopics.ts
@@ -2,6 +2,6 @@ import { instance } from './instance';
 import { TopicData } from 'types/topic';
 
 export const getTopicsData = async () => {
-  const { data } = await instance.get<TopicData[]>('/topics');
+  const { data } = await instance.get<TopicData[]>('/api/topics');
   return data;
 };
