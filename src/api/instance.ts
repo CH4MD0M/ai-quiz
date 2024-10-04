ab4e25ecee025aa2d644ae45522e8c526dc88efb
KiHoon Roh <started677@gmail.com>
2024-10-04 11:13:11 +0900

    update: api url 변경

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.NODE_ENV === 'development' ? '' : process.env.REACT_APP_API_BASE_URL;
+const baseURL = process.env.NODE_ENV === 'development' ? '' : process.env.VITE_API_BASE_URL;
 
 export const instance = axios.create({
   baseURL,
