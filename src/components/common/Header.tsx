48cd2fbeb0ddf23247a8f872554348db1b773fa4
KiHoon Roh <started677@gmail.com>
2024-10-06 22:01:26 +0900

    update: Home page 디자인 변경

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,11 +2,15 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header className="sticky top-0 z-10 w-full bg-blue-200 shadow-md">
-      <div className="container mx-auto px-4 py-[1rem]">
+    <header className="sticky top-0 z-10 h-[8rem] w-full bg-white shadow-[0px_5px_5px_0px_rgba(0,0,0,0.05)]">
+      <div className="mx-auto flex h-full max-w-[1200px] items-center justify-start px-4 py-[1rem]">
         <Link to="/" className="flex items-center space-x-3">
-          <img src="/assets/logo.png" alt="CSQuizHub Logo" className="h-[4rem] w-auto" />
-          <span className="text-[2.4rem] font-bold text-white">CSQuizHub</span>
+          <img
+            src="https://res.cloudinary.com/dfdnn20e7/image/upload/v1728219007/quiz/logo_psatxv.png"
+            alt="CSQuizHub Logo"
+            className="h-[6rem] w-auto"
+          />
+          <span className="text-[2.4rem] font-bold text-black">CSQuizHub</span>
         </Link>
       </div>
     </header>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,82 @@
+import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
-
 import Button from 'components/atoms/Button';
+
+const containerVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: { staggerChildren: 0.3 },
+  },
+};
+
+const wordVariants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.5 },
+  },
+};
+
+const underlineVariants = {
+  hidden: { width: 0 },
+  visible: {
+    width: '100%',
+    transition: { duration: 1, ease: 'easeInOut' },
+  },
+};
 
 const Home = () => {
   return (
-    <section className="mx-auto w-full max-w-4xl p-8 text-center">
-      <h2 className="mb-4 text-4xl font-bold text-blue-800 sm:text-5xl md:text-[5rem]">
-        AI 퀴즈로
-        <br />
-        당신의 <span className="text-green-500">CS 실력</span>을<br />
-        레벨업하세요
-      </h2>
-      <p className="mb-6 text-gray-600"></p>
+    <section className="mx-auto flex w-full max-w-[1200px] flex-col items-center p-[3rem] pb-0 text-center">
+      <motion.div
+        className="mb-[3rem] flex flex-col items-center justify-center p-4 text-[5rem]"
+        variants={containerVariants}
+        initial="hidden"
+        animate="visible"
+      >
+        <motion.div className="mb-4 flex space-x-[1.5rem] font-bold">
+          <motion.div variants={wordVariants}>CS</motion.div>
+          <motion.div variants={wordVariants}>실력향상을</motion.div>
+          <motion.div variants={wordVariants}>위한</motion.div>
+        </motion.div>
+
+        <motion.div className="mb-2 flex space-x-[1.2rem] font-bold">
+          <motion.div variants={wordVariants}>가장</motion.div>
+          <motion.div className="relative mx-2">
+            <motion.div className="z-10" variants={wordVariants}>
+              스마트한
+            </motion.div>
+            <motion.img
+              src="https://res.cloudinary.com/dfdnn20e7/image/upload/v1728219007/quiz/underline_hqsrco.png"
+              className="absolute bottom-5 -z-10"
+              variants={underlineVariants}
+            />
+          </motion.div>
+          <motion.div variants={wordVariants}>선택,</motion.div>
+          <motion.div variants={wordVariants} className="font-bold text-blue-600">
+            CSQuizHub
+          </motion.div>
+        </motion.div>
+      </motion.div>
+
       <Link to="/select-topics">
-        <Button variant="primary" size="md">
+        <Button
+          variant="outline"
+          size="md"
+          className="border-2 border-gray-800 bg-white px-[5rem] py-[2rem] text-gray-800 transition-all hover:-translate-y-2 hover:bg-black hover:text-white"
+        >
           시작하기
         </Button>
       </Link>
+
+      <div className="mt-[9rem] max-w-[800px]">
+        <img
+          src="https://res.cloudinary.com/dfdnn20e7/image/upload/v1728219008/quiz/hero_qs4gaz.jpg"
+          alt="hero-image"
+        />
+      </div>
     </section>
   );
 };
