4aef978a3c62335131bf008c5e880cb4bc4e8a2a
KiHoon Roh <started677@gmail.com>
2024-10-06 13:27:02 +0900

    fix: useEffect 최적화로 불필요한 렌더링 방지

--- src/components/QuestionType/FillInTheBlank.tsx
+++ src/components/QuestionType/FillInTheBlank.tsx
@@ -10,7 +10,7 @@ interface FillInTheBlankQuestionProps {
 
 const FillInTheBlankQuestion = ({ quizData }: FillInTheBlankQuestionProps) => {
   const { question, userAnswer } = quizData;
-  const { currentQuestionIndex, quizDataList, setUserAnswer } = useQuizStore();
+  const { currentQuestionIndex, setUserAnswer } = useQuizStore();
   const [inputValue, setInputValue] = useState((userAnswer as string) || '');
   const debouncedInputValue = useDebounce(inputValue, 300);
   const inputRef = useRef<HTMLInputElement>(null);
@@ -21,27 +21,27 @@ const FillInTheBlankQuestion = ({ quizData }: FillInTheBlankQuestionProps) => {
 
   // Update GLOBAL state when the debounced input value changes
   useEffect(() => {
-    setUserAnswer(currentQuestionIndex, debouncedInputValue);
+    if (debouncedInputValue !== userAnswer) {
+      setUserAnswer(currentQuestionIndex, debouncedInputValue);
+    }
     inputRef.current?.focus();
-  }, [debouncedInputValue]);
-
-  // Synchronize LOCAL input value with global state when the question changes
-  useEffect(() => {
-    setInputValue((quizDataList[currentQuestionIndex].userAnswer as string) || '');
-  }, [currentQuestionIndex]);
+  }, [debouncedInputValue, currentQuestionIndex]);
 
   return (
-    <div className="space-y-4">
-      <h3 className="text-lg font-semibold">{question}</h3>
+    <>
+      <span className="text-[2.2rem] font-semibold text-indigo-700">
+        Q{currentQuestionIndex + 1}
+      </span>
+      <h3 className="text-[2rem] font-normal">{question}</h3>
       <input
         type="text"
         ref={inputRef}
         value={inputValue}
         onChange={handleInputChange}
         placeholder="답변을 입력하세요"
-        className="w-full rounded-lg border border-gray-300 p-3 focus:outline-none focus:ring-2 focus:ring-blue-600"
+        className="mt-[3rem] w-full rounded-lg border border-gray-300 p-3 focus:outline-none focus:ring-2 focus:ring-blue-600"
       />
-    </div>
+    </>
   );
 };
 
